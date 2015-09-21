fbddc8e01131efb683b2ccea1676a0ab55ab6a8b
51103220 <rewandedo1009@gmail.com>
2015-09-21 17:53:49 +0700

    Init

---
+++ data_handler.rb
@@ -0,0 +1,37 @@
+require 'roo'
+require 'yaml'
+def mapper_handler
+	file = Roo::Spreadsheet.open('./sparc.ods')
+	f = file.sheet(0)
+	sprac_data = []
+	f.each(opcode: 'Opcode', mnemonic: 'Mnemonic',parameters: 'Argument List') do |hash|
+		if hash[:opcode].nil? == true
+			hash[:opcode] = ""
+		end
+		if hash[:mnemonic].nil? == true
+			hash[:mnemonic] = ""
+		end
+		if hash[:parameters].nil? == true
+			hash[:parameters] = ""
+		end
+		sprac_data << Hash[:opcode => hash[:opcode].gsub(/\s+/, ""),
+						   :mnemonic => hash[:mnemonic].gsub(/\s+/, ""),
+						   :parameters => hash[:parameters].gsub(/\s+/, "").split(',')
+						   ]
+	end
+	File.open("./input/sparcmapper", "w") do |file|
+		file.puts sprac_data.to_yaml
+	end
+end
+
+def transform_handler
+	sparc_transform = [
+			[:name => "rs1" , :function => "dis_Register(*)"],
+			[:name => "rd" , :function => "dis_Register(*)"],
+			[:name => "roi" , :function => "dis_Number(*)"],
+			[:name => "imm22" , :function => "dis_Number(*)"]
+		]
+	File.open("./input/sparctransform", "w") do |file|
+		file.puts sparc_transform.to_yaml
+	end	
+end
---
+++ decoderParser.rb
@@ -0,0 +1,58 @@
+require './mParse'
+require 'yaml'
+
+class DecoderParser
+	def initialize (content)
+		@content = content
+		@machine = "unknown"
+	end
+	def read_decoder_file file
+		File.open(file).each do |line|
+			if line =~ /#include./ 
+				@content << line 
+			end
+		end
+		instructions = parse_inst (file)
+		instructions.each do |inst|
+			@mapper.each do |m|
+				if m[:mnemonic].downcase =~ /#{inst[:name].downcase}/
+					opcode = m[:opcode]
+
+					@content << "if(lines.at(0) == #{opcode}){\n"
+					@content << m[:mnemonic] << "\n"
+					@content << inst[:codes] << "}\n"
+
+				end
+			end
+			
+		end
+	end
+
+	def read_instruction_file file
+		begin
+			@mapper = YAML.load(File.read(file))
+		rescue
+			raise "Can't load #{@machine}mapper"
+		end
+	end
+
+	def parse file
+		if file =~ /\.\/input\/(?<machine>[a-zA-Z0-9_]+)decoder.m/
+			@machine = file.match(/\.\/input\/(?<machine>[a-zA-Z0-9_]+)decoder.m/)[:machine]
+		end
+		read_instruction_file "./input/#{@machine}mapper"
+		read_decoder_file(file)
+		output = "./output/#{@machine}AssembllyDecoder.cpp"
+		write_to_output(output)
+	end
+
+	def write_to_output file
+		File.open(file, "w") do |f|
+			f.puts @content
+		end
+	end
+	private :read_decoder_file, :read_instruction_file, :write_to_output
+end
+
+parser = DecoderParser.new "\/*Sparc Assemblly Decoder automatically generated*\/\n"
+parser.parse "./input/sparcdecoder.m"
